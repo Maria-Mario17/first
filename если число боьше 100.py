3ac6c0d9aff2a1ffd74377116265ffc4d99bf6fb
Maria-Mario17 <dmitrieva_maria.1710@mail.ru>
2025-06-13 14:37:13 +0300

    Add files via upload

---
+++ если число боьше 100.py
@@ -0,0 +1,6 @@
+a = 100
+b = 117
+if b > a:
+    print('b больше a')
+else:
+    print('b не юольшеa')
---
+++ число меньше 50.py
@@ -0,0 +1,7 @@
+a = 50
+b = 7
+
+if b < a:
+    print('b меньше a')
+else:
+    print('b больше a')
